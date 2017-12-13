78d6c96ce19da235ccd1e42d0e888c63b982de71
Mohannad <mohannadmostafa@outlook.com>
2017-12-13 13:13:41 -0500

    fix small logical and typo mistakes

--- recording_alg.py
+++ recording_alg.py
@@ -1,3 +1,5 @@
+import math
+
 """
     wmax(k) is the largest integer w <= WMAX such that two conditions are satisified:
     1. di < k,
@@ -17,7 +19,7 @@
 """
 def wmax(k, Wn, D):
     w = Wn + 2;
-    for i in range(Wn + 2, -1, 1):
+    for i in range(Wn + 2, 1, -1):
         d_flag = True;
         Dw_neg = get_Dw_Neg(D, i);
         # check the first candidate where di \in D < k
@@ -60,7 +62,8 @@ def get_Dw(D, w):
     Dw = [];
     for d in D:
         Dw.append(pw(d, w))
-    return Dw.sort();
+    Dw.sort();
+    return Dw;
 
 """
     Calculcate the set D_w' by using Dw
@@ -91,32 +94,39 @@ def get_Dw_Neg(D, w):
 
 
 def get_Wn(D):
-    return math.floor(math.log(max(D), 2));
+    return int(math.floor(math.log(max(D), 2)));
 
 def digitD(k, D):
     if k % 2 == 0:
         return 0;
+    if k == 1:
+        return k;
     Wn = get_Wn(D);
-    Wmax = get_Wn(k, Wn, D);
+    Wmax = wmax(k, Wn, D);
     Dwmax = get_Dw(D, Wmax);
-    result = null;
+    result = -100;
+    flag_cond1 = False;
     if pw(k, Wmax) in Dwmax:
         for d in D:
             if d >= k:
                 continue;
             if pw(k, Wmax) == pw(d, Wmax):
                 result = d;
+                # this flag is to check if it's need to go and check the other condition.
+                flag_cond1 = True;
                 break;
-        return result;
+        if flag_cond1:
+            return result;
 
     if (2**Wmax - pw(k, Wmax)) in Dwmax:
         for d in D:
             if d >= k:
                 continue;
-            if (2**Wmax - pw(k, Wmax)) == p(d, Wmax):
+            if (2**Wmax - pw(k, Wmax)) == pw(d, Wmax):
                 result = -d;
-
         return result;
+
+    print "no condition"
 
 
 def RDP(k, D):
@@ -127,3 +137,9 @@ def RDP(k, D):
         k = (k - ki) / 2;
 
     return bin_k;
+
+if __name__ == '__main__':
+    k = 31415;
+    D = [1, 3, 23, 27];
+    result = RDP(k, D);
+    print result;
