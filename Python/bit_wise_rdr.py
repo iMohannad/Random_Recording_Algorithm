b1174df7ad419882811101641e0cdbc4c87fa381
Mohannad <mohannadmostafa@outlook.com>
2018-04-12 17:11:29 -0400

    implement a while loop to check for zeros

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -10,22 +10,33 @@ def main():
     rdr = []
     bin_k = bin(k)[2:]
     Wn = get_Wn(D)
-    while k != 0:
-
-        for d in D:
-            if d < k:
-                bin_d = bin(d)[2:]
-                length_bin_d = len(bin(d))
-                # Neg D
-                neg_d = 2**i - d
-                neg_bin_d = bin(neg_d)[2:]
-                length_neg_bin_d = len(neg_bin_d)
-                if bin_d ^ bin_k[len(bin_k)-length_bin_d-1:] == 0:
-                    rdr.insert(0, d)
-                    break
-                if neg_bin_d ^ bin_k[len(bin_k)-length_neg_bin_d-1:] == 0:
-                    rdr.insert(0, -d)
-                    break
+    flag_d = 0
+    while bin_k != '':
+        if bin_k[len(bin_k)-1] == 0:
+            rdr.insert(0, 0)
+            bin_k = bin_k[:len(bin_k)-1]
+            continue
+        for i in range(Wn + 2, 2, -1):
+            for d in D:
+                if d < k:
+                    bin_d = bin(d)[2:]
+                    length_bin_d = len(bin(d))
+                    # Neg D
+                    neg_d = 2**i - d
+                    neg_bin_d = bin(neg_d)[2:]
+                    length_neg_bin_d = len(neg_bin_d)
+                    if int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d-1:], 2) == 0:
+                        rdr.insert(0, d)
+                        bin_k = bin_k[:len(bin_k)-length_bin_d]
+                        flag_d = 1
+                        break
+                    if int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d-1:], 2) == 0:
+                        rdr.insert(0, -d)
+                        bin_k = bin_k[:len(bin_k)-length_neg_bin_d-1]
+                        flag_d = 1
+                        break
+            if flag_d == 1:
+                break
 
 
 if __name__ == '__main__':
