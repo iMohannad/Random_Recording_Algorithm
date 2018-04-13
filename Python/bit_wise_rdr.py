cd5818b3f6cfb7b47019e6db823fdbe5531ac22c
Mohannad <mohannadmostafa@outlook.com>
2018-04-13 16:48:36 -0400

    changing the variable i to s and reorder some lines

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -11,25 +11,24 @@ def main():
     bin_k = bin(k)[2:]
     Wn = get_Wn(D)
     flag_d = 0
-    max_length = Wn + 2
+    max_length = len(bin(max(D)))
     while bin_k != '':
         if bin_k[len(bin_k)-1] == '0':
             rdr.insert(0, 0)
             bin_k = bin_k[:len(bin_k)-1]
             continue
-        for i in range(Wn + 2, 2, -1):
+        for s in range(Wn + 2, 2, -1):
             for d in D:
+                bin_d = bin(d)[2:]
+                length_bin_d = len(bin(d))
+                # Neg D
+                neg_d = 2**s - d
+                # print "neg_d = ", neg_d, " i = ", s, " d = ", d
+                neg_bin_d = bin(neg_d)[2:]
+                # print "neg_bin_d = ", neg_bin_d
+                length_neg_bin_d = len(neg_bin_d)
                 if d < k:
-                    bin_d = bin(d)[2:]
-                    print "bin_d = ", bin_d
-                    length_bin_d = len(bin(d))
-                    # Neg D
-                    neg_d = 2**i - d
-                    print "neg_d = ", neg_d, " i = ", i, " d = ", d
-                    neg_bin_d = bin(neg_d)[2:]
-                    print "neg_bin_d = ", neg_bin_d
-                    length_neg_bin_d = len(neg_bin_d)
-                    if int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
+                    if max_length == length_bin_d and int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
                         rdr.insert(0, d)
                         bin_k = bin_k[:len(bin_k)-length_bin_d]
                         flag_d = 1
