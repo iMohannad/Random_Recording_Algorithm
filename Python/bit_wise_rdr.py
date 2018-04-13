ef65a8f01d39cceb74b4c11a2cfdc94d97f77776
Mohannad <mohannadmostafa@outlook.com>
2018-04-13 12:50:02 -0400

    adding some print statements to check the data

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -11,6 +11,7 @@ def main():
     bin_k = bin(k)[2:]
     Wn = get_Wn(D)
     flag_d = 0
+    max_length = bin(max(D))
     while bin_k != '':
         if bin_k[len(bin_k)-1] == 0:
             rdr.insert(0, 0)
@@ -20,22 +21,26 @@ def main():
             for d in D:
                 if d < k:
                     bin_d = bin(d)[2:]
+                    print "bin_d = ", bin_d
                     length_bin_d = len(bin(d))
                     # Neg D
                     neg_d = 2**i - d
+                    print "neg_d = ", neg_d, " i = ", i, " d = ", d
                     neg_bin_d = bin(neg_d)[2:]
+                    print "neg_bin_d = ", neg_bin_d
                     length_neg_bin_d = len(neg_bin_d)
-                    if int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d-1:], 2) == 0:
+                    if int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
                         rdr.insert(0, d)
                         bin_k = bin_k[:len(bin_k)-length_bin_d]
                         flag_d = 1
                         break
-                    if int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d-1:], 2) == 0:
+                    if int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0:
                         rdr.insert(0, -d)
                         bin_k = bin_k[:len(bin_k)-length_neg_bin_d-1]
                         flag_d = 1
                         break
             if flag_d == 1:
+                flag_d = 0
                 break
 
 
