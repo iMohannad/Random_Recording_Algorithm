e3bfd02e937c4edd75ff1b256b3ae54b8ee2e5bf
Mohannad <mohannadmostafa@outlook.com>
2018-04-16 17:53:32 -0400

    fix the if condition and string assignments

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -25,7 +25,7 @@ def main():
                 bin_k = ''
                 print rdr
                 break
-            for d in D:
+            for d in D[::-1]:
                 print "-----------------------new d-----------------------"
                 bin_d = bin(d)[2:]
                 length_bin_d = len(bin_d)
@@ -38,7 +38,7 @@ def main():
                 neg_bin_d = bin(neg_d)[2:]
                 print "neg_bin_d = ", neg_bin_d
                 length_neg_bin_d = len(neg_bin_d)
-                if bin_d < bin_k:
+                if bin_d <= bin_k:
                     print "len bin_k > ", len(bin_k), " length of d = ", length_bin_d
                     print "int d = ", int(bin_d, 2), ", d = ", d, " bin_k = ", int(bin_k[len(bin_k)-length_bin_d-1:], 2)
                     if length_bin_d == (s-1) and int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
@@ -50,18 +50,33 @@ def main():
                         print "update bin_k > ", bin_k
                         flag_d = 1
                         break
-                    elif length_neg_bin_d == (s-1) and int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0:
+                    elif int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0 or int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 2**(s-1):
                         rdr.insert(0, -d)
-                        carry = 0
-                        bin_k = bin_k[:len(bin_k)-length_neg_bin_d]
-                        if bin_k[len(bin_k)-1] == 1:
-                            carry = 1
+                        # Inserting zeros
+                        for j in range(0, length_neg_bin_d):
+                            rdr.insert(0, 0)
+
+                        carry = '0'
+                        # Find number of zeros in the beginning
+                        
+                        bin_k = bin_k[:len(bin_k)-(s-1)]
+                        print "bin_k > ", bin_k
+                        bin_s = list(bin_k)
+                        print bin_s
+                        print bin_s[len(bin_k)-1]
+                        if bin_s[len(bin_k)-1] == '1':
+                            carry = '1'
                             index =len(bin_k)-1
-                        while carry == 1:
-                            carry = bin_k[index] & carry
-                            bin_k[index] = bin_k[index] ^ carry
+                        else:
+                            bin_s[len(bin_k)-1] = '1'
+                        
+                        while carry == '1':
+                            carry = '1' if bin_s[index] == '1' else '0'
+                            bin_s[index] = '0' if int(bin_s[index],2) ^ int(carry,2) == 0 else '1'
                             index = index - 1
                         flag_d = 1
+                        bin_k = "".join(bin_s)
+                        print "updated bin_k > ", bin_k
                         break
                     max_length = max_length - 1
             if flag_d == 1:
