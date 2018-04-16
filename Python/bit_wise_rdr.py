d2ccdef6190865fb0ce52f0d72709e25ac62425d
Mohannad <mohannadmostafa@outlook.com>
2018-04-16 13:56:08 -0400

    Fill the rest with zeros if the digit matches

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -12,47 +12,60 @@ def main():
     Wn = get_Wn(D)
     flag_d = 0
     max_length = len(bin(max(D)))
+    count = 0
     while bin_k != '':
+        # Remove leading zeros
         if bin_k[len(bin_k)-1] == '0':
             rdr.insert(0, 0)
             bin_k = bin_k[:len(bin_k)-1]
             continue
         for s in range(Wn + 2, 2, -1):
+            count = count + 1
+            if count == 10:
+                bin_k = ''
+                print rdr
+                break
             for d in D:
+                print "-----------------------new d-----------------------"
                 bin_d = bin(d)[2:]
-                length_bin_d = len(bin(d))
+                length_bin_d = len(bin_d)
+                print "bin d > ", bin_d, " length_bin_d > ", length_bin_d
                 # Neg D
                 neg_d = 2**s - d
-                # print "neg_d = ", neg_d, " i = ", s, " d = ", d
+                while neg_d < 0:
+                    neg_d = neg_d + 2**s
+                print "neg_d = ", neg_d, " s = ", s, " d = ", d
                 neg_bin_d = bin(neg_d)[2:]
-                # print "neg_bin_d = ", neg_bin_d
+                print "neg_bin_d = ", neg_bin_d
                 length_neg_bin_d = len(neg_bin_d)
-                if d < k:
-                    bin_d = bin(d)[2:]
-                    print "bin_d = ", bin_d
-                    length_bin_d = len(bin(d))
-                    # Neg D
-                    neg_d = 2**i - d
-                    while neg_d < 0:
-                        neg_d = neg_d + 2**i
-                    print "neg_d = ", neg_d, " i = ", i, " d = ", d
-                    neg_bin_d = bin(neg_d)[2:]
-                    print "neg_bin_d = ", neg_bin_d
-                    length_neg_bin_d = len(neg_bin_d)
-                    if length_bin_d == max_length and int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
+                if bin_d < bin_k:
+                    print "len bin_k > ", len(bin_k), " length of d = ", length_bin_d
+                    print "int d = ", int(bin_d, 2), ", d = ", d, " bin_k = ", int(bin_k[len(bin_k)-length_bin_d-1:], 2)
+                    if length_bin_d == (s-1) and int(bin_d, 2) ^ int(bin_k[len(bin_k)-length_bin_d:], 2) == 0:
                         rdr.insert(0, d)
+                        print "it went innnnnnnnnnnnnnn"
+                        for j in range(0, length_bin_d):
+                            rdr.insert(0, 0)
                         bin_k = bin_k[:len(bin_k)-length_bin_d]
                         flag_d = 1
                         break
-                    if length_neg_bin_d == max_length and int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0:
+                    elif length_neg_bin_d == (s-1) and int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0:
                         rdr.insert(0, -d)
-                        bin_k = bin_k[:len(bin_k)-length_neg_bin_d-1]
+                        carry = 0
+                        bin_k = bin_k[:len(bin_k)-length_neg_bin_d]
+                        if bin_k[len(bin_k)-1] == 1:
+                            carry = 1
+                            index =len(bin_k)-1
+                        while carry == 1:
+                            carry = bin_k[index] & carry
+                            bin_k[index] = bin_k[index] ^ carry
+                            index = index - 1
                         flag_d = 1
                         break
                     max_length = max_length - 1
             if flag_d == 1:
                 flag_d = 0
-                max_length = bin(max(D))
+                s = Wn + 2
                 break
     print "Final RDR = ", rdr
 
