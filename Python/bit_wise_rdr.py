46cc31d825fd03cc1939eef44d52162491912d3c
Mohannad <mohannadmostafa@outlook.com>
2018-04-25 02:27:42 -0400

    Write a function to add carry to the function

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -1,16 +1,73 @@
 import math
 
+def add_carry(bin_k, len_k, s, length_neg_bin_d, rdr):
+    bin_s = list(bin_k)
+    print bin_s
+    print "bin_s[", len(bin_k)-s-1,"] > ",bin_s[len(bin_k)-s-1]
+    carry = '0'
+    length_bin_s = len(bin_s)
+    # Check up the carry
+    # Index of next bit
+    index =len(bin_k)-s-2
+    if (len(bin_k)-s-1) == -1:
+        if (s == length_neg_bin_d):
+            rdr.insert(0, 1)
+            flag_d = 1
+            return ''
+        else:
+            flag_d = 1
+            bin_s.insert(0, '1')
+            bin_s = bin_s[:len(bin_s) - length_neg_bin_d]
+            bin_k = "".join(bin_s)
+            return bin_k
+    if bin_s[len(bin_k)-s-1] == '1':
+        carry = '1'
+        bin_s[len(bin_k)-s-1] = '0'
+        print "bin_s updated > ", bin_s, " len(bin_k)-s ", len(bin_k)-s
+    else:
+        bin_s[len(bin_k)-s-1] = '1'
+
+    while carry == '1':
+        print index
+
+        if index == 0:
+            if (bin_s[index] == '1'):
+                bin_s[index] = '0'
+                bin_s.insert(0, '1')
+                carry = '0'
+            else:
+                bin_s[index] = '1'
+            break
+        carry = '1' if bin_s[index] == '1' else '0'
+        bin_s[index] = '0' if bin_s[index] == '1' else '1'
+        index = index - 1
+
+    # Find number of zeros in the beginning
+    # index = 0
+    # while int(bin_s[len(bin_s)-index-1], 2) ^ int(neg_bin_d[len(neg_bin_d)-index-1]) == 0:
+    #     rdr.insert(0, 0)
+    #     index = index+1
+    flag_d = 1
+    bin_s = bin_s[:len(bin_s) - length_neg_bin_d]
+    bin_k = "".join(bin_s)
+    print "updated binary s> ", bin_s
+    print "updated bin_k > ", bin_k
+    print "RDR > ", rdr
+    # bin_k = bin_k[:len(bin_s)-(s-1)]
+    print "bin_k > ", bin_k
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    return bin_k
 
 def get_Wn(D):
     return int(math.floor(math.log(max(D), 2)))
 
 def main():
-    # D = [1, 3, 23, 27]
+    D = [1, 3, 23, 27]
     # D = [1, 17, 21, 25, 27]
     # D = [1, 9, 13, 17, 21]
     # D = [1, 3, 5, 11, 19]
     # D =  [1, 9, 11, 27, 29]
-    D =  [1, 3, 9, 15, 19]
+    # D =  [1, 3, 9, 15, 19]
     # D =  [1, 5, 19, 25, 27]
     # D =  [1, 9, 15, 17, 23] # Gives me problems .. problems solved
     # D =  [1, 3, 9, 11, 29]
@@ -81,75 +138,17 @@ def main():
                         print "length neg_bin d > ", length_neg_bin_d
                         for j in range(0, length_neg_bin_d-1):
                             rdr.insert(0, 0)
-
-                        bin_s = list(bin_k)
-                        print bin_s
-                        print "bin_s[", len(bin_k)-s-1,"] > ",bin_s[len(bin_k)-s-1]
-                        carry = '0'
-                        length_bin_s = len(bin_s)
-                        # Check up the carry
-                        # Index of next bit
-                        index =len(bin_k)-s-2
-                        if (len(bin_k)-s-1) == -1:
-                            if (s == length_neg_bin_d):
-                                rdr.insert(0, 1)
-                                flag_d = 1
-                                bin_k = ''
-                                break
-                            else:
-                                flag_d = 1
-                                bin_s.insert(0, '1')
-                                bin_s = bin_s[:len(bin_s) - length_neg_bin_d]
-                                bin_k = "".join(bin_s)
-                                break
-                        if bin_s[len(bin_k)-s-1] == '1':
-                            carry = '1'
-                            bin_s[len(bin_k)-s-1] = '0'
-                            print "bin_s updated > ", bin_s, " len(bin_k)-s ", len(bin_k)-s
-                        else:
-                            bin_s[len(bin_k)-s-1] = '1'
-
-                        while carry == '1':
-                            print index
-
-                            if index == 0:
-                                if (bin_s[index] == '1'):
-                                    bin_s[index] = '0'
-                                    bin_s.insert(0, '1')
-                                    carry = '0'
-                                else:
-                                    bin_s[index] = '1'
-                                break
-                            carry = '1' if bin_s[index] == '1' else '0'
-                            bin_s[index] = '0' if bin_s[index] == '1' else '1'
-                            index = index - 1
-
-                        # Find number of zeros in the beginning
-                        # index = 0
-                        # while int(bin_s[len(bin_s)-index-1], 2) ^ int(neg_bin_d[len(neg_bin_d)-index-1]) == 0:
-                        #     rdr.insert(0, 0)
-                        #     index = index+1
+                        bin_k = add_carry(bin_k, len(bin_k), s, length_neg_bin_d, rdr)
                         flag_d = 1
-                        bin_s = bin_s[:len(bin_s) - length_neg_bin_d]
-                        bin_k = "".join(bin_s)
-                        print "updated binary s> ", bin_s
-                        print "updated bin_k > ", bin_k
-                        print "RDR > ", rdr
-                        # bin_k = bin_k[:len(bin_s)-(s-1)]
-                        print "bin_k > ", bin_k
-                        print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
                         break
                     max_length = max_length - 1
             if flag_d == 1:
                 flag_d = 0
                 s = Wn + 2
                 break
-        print "S >>>>>>>>>> ", s
         if flag_d == 0 and s == 2:
             rdr.insert(0, 1)
-            print rdr, " < UPDATEDDDDDDDDDDDDDDDD"
             bin_k = bin_k[:len(bin_k)-1]
-            print "UPDATED BIN_K > ", bin_k
     print "Final RDR = ", rdr
 
 if __name__ == '__main__':
