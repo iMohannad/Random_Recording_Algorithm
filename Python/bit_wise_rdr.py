e4c55e4627dffd41a962cfa3ab73f8cb3f19f612
Mohannad <mohannadmostafa@outlook.com>
2018-04-26 19:24:03 -0400

    Remove printing statments and add a function to run tests

--- Python/bit_wise_rdr.py
+++ Python/bit_wise_rdr.py
@@ -1,4 +1,6 @@
 import math
+import random
+import time
 
 def generate_random_D(m, l):
     if l > (m+1)/2:
@@ -16,8 +18,8 @@ def generate_random_D(m, l):
 
 def add_carry(bin_k, len_k, s, length_neg_bin_d, rdr):
     bin_s = list(bin_k)
-    print bin_s
-    print "bin_s[", len(bin_k)-s-1,"] > ",bin_s[len(bin_k)-s-1]
+    # print bin_s
+    # print "bin_s[", len(bin_k)-s-1,"] > ",bin_s[len(bin_k)-s-1]
     carry = '0'
     length_bin_s = len(bin_s)
     # Check up the carry
@@ -37,12 +39,12 @@ def add_carry(bin_k, len_k, s, length_neg_bin_d, rdr):
     if bin_s[len(bin_k)-s-1] == '1':
         carry = '1'
         bin_s[len(bin_k)-s-1] = '0'
-        print "bin_s updated > ", bin_s, " len(bin_k)-s ", len(bin_k)-s
+        # print "bin_s updated > ", bin_s, " len(bin_k)-s ", len(bin_k)-s
     else:
         bin_s[len(bin_k)-s-1] = '1'
 
     while carry == '1':
-        print index
+        # print index
 
         if index == 0:
             if (bin_s[index] == '1'):
@@ -64,19 +66,19 @@ def add_carry(bin_k, len_k, s, length_neg_bin_d, rdr):
     flag_d = 1
     bin_s = bin_s[:len(bin_s) - length_neg_bin_d]
     bin_k = "".join(bin_s)
-    print "updated binary s> ", bin_s
-    print "updated bin_k > ", bin_k
-    print "RDR > ", rdr
+    # print "updated binary s> ", bin_s
+    # print "updated bin_k > ", bin_k
+    # print "RDR > ", rdr
     # bin_k = bin_k[:len(bin_s)-(s-1)]
-    print "bin_k > ", bin_k
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    # print "bin_k > ", bin_k
+    # print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
     return bin_k
 
 def get_Wn(D):
     return int(math.floor(math.log(max(D), 2)))
 
-def main():
-    D = [1, 3, 23, 27]
+def RDR_algorithm(D, k):
+    # D = [1, 3, 23, 27]
     # D = [1, 17, 21, 25, 27]
     # D = [1, 9, 13, 17, 21]
     # D = [1, 3, 5, 11, 19]
@@ -85,7 +87,7 @@ def main():
     # D =  [1, 5, 19, 25, 27]
     # D =  [1, 9, 15, 17, 23] # Gives me problems .. problems solved
     # D =  [1, 3, 9, 11, 29]
-    k = 31415
+    # k = 31415
     rdr = []
     bin_k = bin(k)[2:]
     Wn = get_Wn(D)
@@ -97,61 +99,61 @@ def main():
         if bin_k[len(bin_k)-1] == '0':
             rdr.insert(0, 0)
             bin_k = bin_k[:len(bin_k)-1]
-            print rdr
+            # print rdr
             continue
         for s in range(Wn + 2, 1, -1):
             count = count + 1
-            if count >= 20:
-                bin_k = ''
-                print "COUNT REACHED"
-                flag_d = 1
-                print rdr
-                break
+            # if count >= 20:
+            #     bin_k = ''
+            #     print "COUNT REACHED"
+            #     flag_d = 1
+            #     print rdr
+            #     break
             for d in D:
-                print "-----------------------new d-----------------------"
+                # print "-----------------------new d-----------------------"
                 bin_d = bin(d)[2:]
                 length_bin_d = len(bin_d)
-                print "D > ", d, " bin d > ", bin_d, " length_bin_d > ", length_bin_d
+                # print "D > ", d, " bin d > ", bin_d, " length_bin_d > ", length_bin_d
                 # Neg D
                 neg_d = 2**s - d
                 while neg_d < 0:
                     neg_d = neg_d + 2**s
-                print "neg_d = ", neg_d, " s = ", s,
+                # print "neg_d = ", neg_d, " s = ", s,
                 neg_bin_d = bin(neg_d)[2:]
-                print " neg_bin_d = ", neg_bin_d
+                # print " neg_bin_d = ", neg_bin_d
                 length_neg_bin_d = len(neg_bin_d)
-                print "bin_d> ", bin_d, " bin_k > ", bin_k, "bin_d < bin_k = ", bin_d <= bin_k
+                # print "bin_d> ", bin_d, " bin_k > ", bin_k, "bin_d < bin_k = ", bin_d <= bin_k
                 if bin_d <= bin_k and length_bin_d <= len(bin_k):
-                    print "----------------------bin_d less than bin_k"
+                    # print "----------------------bin_d less than bin_k"
                     # If d value equal to k
                     if int(bin_d, 2) ^ int(bin_k, 2) == 0:
                         rdr.insert(0, d)
-                        print rdr
+                        # print rdr
                         bin_k = ''
                         flag_d = 1
                         break
-                    print bin_k[len(bin_k) - s:]
-                    print "neg bin_d > ", int(neg_bin_d, 2) , " bin_k > ", int(bin_k[len(bin_k)-s:], 2), " Value > ", int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-s:], 2)
-                    print "len bin_k > ", len(bin_k), " length of d = ", length_bin_d
-                    print "int d = ", int(bin_d, 2), ", d = ", d, " bin_k = ", int(bin_k[len(bin_k)-length_bin_d-1:], 2)
-                    print "length of bin_d", len(bin_d[length_bin_d-s:])
+                    # print bin_k[len(bin_k) - s:]
+                    # print "neg bin_d > ", int(neg_bin_d, 2) , " bin_k > ", int(bin_k[len(bin_k)-s:], 2), " Value > ", int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-s:], 2)
+                    # print "len bin_k > ", len(bin_k), " length of d = ", length_bin_d
+                    # print "int d = ", int(bin_d, 2), ", d = ", d, " bin_k = ", int(bin_k[len(bin_k)-length_bin_d-1:], 2)
+                    # print "length of bin_d", len(bin_d[length_bin_d-s:])
                     if len(bin_d[length_bin_d-s:]) == (s) and int(bin_d, 2) ^ int(bin_k[len(bin_k)-(s):], 2) == 0:
                         rdr.insert(0, d)
-                        print "it went innnnnnnnnnnnnnn"
+                        # print "it went innnnnnnnnnnnnnn"
                         for j in range(0, length_bin_d-1):
                             rdr.insert(0, 0)
                         bin_k = bin_k[:len(bin_k)-length_bin_d]
-                        print "update bin_k > ", bin_k
-                        print "RDR > ", rdr
+                        # print "update bin_k > ", bin_k
+                        # print "RDR > ", rdr
                         flag_d = 1
                         break
                     elif int(neg_bin_d, 2) ^ int(bin_k[len(bin_k)-length_neg_bin_d:], 2) == 0 and neg_d != 1:
                         if bin_k[len(bin_k)-s-1] == '1':
                             continue
-                        print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                        # print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
                         rdr.insert(0, -d)
                         # Inserting zeros
-                        print "length neg_bin d > ", length_neg_bin_d
+                        # print "length neg_bin d > ", length_neg_bin_d
                         for j in range(0, length_neg_bin_d-1):
                             rdr.insert(0, 0)
                         bin_k = add_carry(bin_k, len(bin_k), s, length_neg_bin_d, rdr)
@@ -165,24 +167,23 @@ def main():
         if flag_d == 0 and s == 2:
             rdr.insert(0, 1)
             bin_k = bin_k[:len(bin_k)-1]
-    print "Final RDR = ", rdr
+    # print "Final RDR = ", rdr
+    return [rdr, len(rdr)]
 
 def run_tests_time():
+    min_len = 1000
     i = 5
-    [D, Di, naf, min_length] = RDR(1000, i, 26959956671506397946670150870196259404578077144243917216827126959956671506397946670150870196259404578077144243917216)
-    min_len = min_length
-    D_set = D
-    D_result = Di
-    naf_result = naf
     j = 0
     averageTime = 0
     while i <= 300:
         while j < 1000:
+            D = generate_random_D(i*2, i)
             startTime = time.time()
-            [D, Di, naf, min_length] = RDR(1000, i, 269599566715063979466701508701962594045780726959956671506397946670150870196259404578077144243917216827126959956671506397946670150870196259404578077144243917216714424391721682712368051)
+            [rdr, min_length] = RDR_algorithm(D, 2695995667150639794667015087019625940457807714424391721682712368051)
             endTime = time.time()
             averageTime = averageTime + (endTime - startTime)
             j = j+1
+            # print "RDR > ", rdr, " min_length > ", min_length
         averageTime = averageTime / 1000
         print "Average Time for digit set of Size ", i, " = ", averageTime
         averageTime = 0
@@ -190,4 +191,4 @@ def run_tests_time():
         i = i+1
 
 if __name__ == '__main__':
-    main()
+    run_tests_time()
