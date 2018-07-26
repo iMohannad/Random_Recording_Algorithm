1f6ab0033d86cceef448286ac2755b65960b6d61
Mohannad S. Mostafa <mohannadmostafa@outlook.com>
2018-07-25 20:48:45 -0400

    add a function to generate NAF

--- Comparison/l_naf.py
+++ Comparison/l_naf.py
@@ -1,6 +1,14 @@
 import sys
 
-
+def generate_naf(w):
+    naf_set = []
+    naf_set.append(1)
+    i = 3
+    while (i <= (2**(w-1)):
+        naf_set.append(i)
+        i = i + 2;
+    return naf_set
+    
 def find_naf(k, l, w):
     i = 0
     ki = -7
